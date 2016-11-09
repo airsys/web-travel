51fad089544da08de02af0b3ad2853e3901f709c
Dita Mahardika S <mahardikasuryadi@gmail.com>
2016-11-09 14:59:32 +0700

    searchbox
    searchboc inline (satu garis)

--- application/views/airlines/search.php
+++ application/views/airlines/search.php
@@ -2,35 +2,48 @@
 	<div class="box-header with-border">
 	</div>
 	<form id="form" method="post" name="form">
-		<div class="box-body">
-			<div class="col-md-3">
+		<div class="box-body" style="padding-left: 30px;padding-right: 60px;left: 6%;position: relative;">
+			<div class="col-search3">
 				<div class="form-group">
-					<label>LEAVING FROM</label> <select class="form-control bandara" id='from' name='from' style="width: 100%;">
+					<label>LEAVING FROM</label>
+                    <div class="input-group ">
+                        <div class="input-group-addon" style="background-color: #d2d6de;">
+                            <i class="fa fa-map-marker"></i>
+                        </div> 
+                        <select class="form-control bandara" id='from' name='from' style="width: 101%;" >
 							<option value=""></option>
 						</select>
+                        </div>
 				</div>
 			</div>
-			<div class="col-md-3">
+			<div class="col-search3">
 				<div class="form-group">
-					<label>GOING TO</label> <select class="form-control bandara" id='to' name='to' style="width: 100%;">
-							<option value=""></option>
-						</select>
+					<label>GOING TO</label>
+                    <div class="input-group">
+                        <div class="input-group-addon" style="background-color: #d2d6de;">
+                            <i class="fa fa-map-marker"></i>
+                        </div>
+                                <select class="form-control bandara" id='to' name='to' style="width: 101%;">
+							         <option value=""></option>
+						      </select>
+                    </div>
 				</div>
 			</div>
-			<div class="col-md-2 col-sm-6 col-xs-12" style="padding-right: 0;margin-right: 0;">
+			<div class="col-search2" style="padding-right: 0;margin-right: 0;">
 				<div class="form-group">
 					<label>DEPARTING ON</label>
-					<div class="input-group">
+					<div class="input-group" style="width: 101%;">
 						<div class="input-group date">
-							<div class="input-group-addon">
+							<div class="input-group-addon" style="background-color: #d2d6de;">
 								<i class="fa fa-calendar"></i>
-							</div><input class="form-control pull-right" id="datepicker" name='date' required="" type="text">
+							</div><input class="form-control pull-right" id="datepicker" name='date' required="" type="text" readonly="readonly" 
+                                        style="padding-right: 50px;background-color: white;">
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-2 col-sm-6 col-xs-12" style="padding: 0;margin: 0;">
-				<div class="col-md-4 col-sm-4 col-xs-4">
+			<!-- <div class="col-search2" style="padding: 0;margin: 0;">
+				<div class="col-search4">
 					<label>ADULT</label>
 					<div class="input-group">
 						<select class="form-control" id='adult' name='adult' style="width: 100%;">
@@ -38,7 +51,7 @@
 								1
 							</option>
 							<option>
-								2
+                                2
 							</option>
 							<option>
 								3
@@ -58,7 +71,7 @@
 						</select>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-4 col-xs-4">
+				<div class="col-search4">
 					<label>CHILDREN</label>
 					<div class="input-group">
 						<select class="form-control" id='child' name='child' style="width: 100%;">
@@ -89,7 +102,7 @@
 						</select>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-4 col-xs-4">
+				<div class="col-search4">
 					<label>INFANT</label>
 					<div class="input-group">
 						<select class="form-control" id='infant' name='infant' style="width: 100%;">
@@ -108,10 +121,74 @@
 						</select>
 					</div>
 				</div>
-			</div>
-			<div class="col-md-2 col-sm-4 col-xs-4">
-				<div class="input-group" style="margin-top: 17px;">
-				<button id='btn-search' class="btn btn-flat btn-success btn-lg"><i class="fa fa-search"></i> | SEARCH</button>
+			</div> -->
+           <div class="col-md-1" style="padding-right: 0;margin-right: 0;padding-left: 0;margin-left: 0;">
+                <div class="form-group">
+                    <label>PASSENGER</label>
+                    <div class="input-group margin" style="margin: 0px 0px 0px 0px; width: 50%;">
+                    <i class="fa fa-user" style="
+                                                padding-top: 10px;
+                                                width: 35px;
+                                                height: 34px;
+                                                border-top-width: 1px;
+                                                padding-left: 11px;
+                                                background-color: #d2d6de;"> <!--passanger -->
+                    </i> 
+                    <div class="input-group-btn ">
+                            <button type="button" class="btn btn-flat dropdown-toggle" data-toggle="dropdown" style="padding-right: 0px;padding-top: 0px;padding-bottom: 0px;padding-left: 0px;">
+                                
+                                <input type="text" class="" id="passanger" value="1" name="passanger" style="width: 71px;height: 32px; border-width: 0px 0px 0px 0px;text-align: center" readonly>
+                                <input type="text" class="" id="triggerPass" value="1" name="triggerPass" style="width: 30px;height: 32px; border-width: 0px 0px 0px 0px; margin-left: 0px;text-align: center" hidden>
+                            </button>
+                                 <ul class="dropdown-menu" role="menu">
+                                    <div class="countuser input-group" align="center"style="width:100%;">
+                                            <label for="adult">Adult <strong>12+ th</strong></label><br>
+                                            <input type="button" value="-" onClick="adultmin()" class="btn btn-flat btn-success" onFocus="startUser();" onBlur="stopUser();"/> 
+                                            <input type="text" name="adult" size="5" id="adult" value="1" readonly
+                                                    style="height: 35.22222px;
+                                                            padding-top: 0px;
+                                                            border-width: 0px 0px 0px 0px;
+                                                            text-align: center;
+                                                        "></td>
+                                            <input type="button" value="+" onClick="adultplus()" class="btn btn-flat btn-success" onFocus="startUser();" onBlur="stopUser();" 
+                                            data-toggle="popover" data-trigger="" data-timeout="2000" data-content="Jumlah Dewasa dan Anak tidak boleh melebihi 7"/> 
+
+                                     </div>   
+                                    <div class="countuser input-group" align="center"style="width:100%;">
+                                            <label for="child">Children <strong></strong></label><br>
+                                            <input type="button" value="-" onClick="childmin()" class="btn btn-flat btn-success" onFocus="startUser();" onBlur="stopUser();"/> 
+                                            <input type="text" name="child" size="5" id="child" value="0" readonly
+                                                     style="height: 35.22222px;
+                                                            padding-top: 0px;
+                                                            border-width: 0px 0px 0px 0px;
+                                                            text-align: center;
+                                                        "></td>
+                                            <input type="button" value="+" onClick="childplus()" class="btn btn-flat btn-success" onFocus="startUser();" onBlur="stopUser();" 
+                                            data-toggle="popover" data-trigger="" data-timeout="2000" data-content="Jumlah Dewasa dan Anak tidak boleh melebihi 7"/> 
+
+                                    </div>
+                                     <div class="countuser input-group" align="center"style="width:100%;">
+                                        <label for="infant">Infant <strong></strong></label><br>
+                                        <input type="button" value="-" onClick="infantmin()" class="btn btn-flat btn-success" onFocus="startUser();" onBlur="stopUser();"/> 
+                                        <input type="text" name="infant" size="5" id="infant" value="0" readonly
+                                                     style="height: 35.22222px;
+                                                            padding-top: 0px;
+                                                            border-width: 0px 0px 0px 0px;
+                                                            text-align: center;
+                                                        "></td>
+                                        <input type="button" value="+" onClick="infantplus()" class="btn btn-flat btn-success"  onFocus="startUser();" onBlur="stopUser();" 
+                                        data-toggle="popover" data-trigger="" data-timeout="1000" data-content="Jumlah bayi tidak boleh melebihi jumlah dewasa"/> 
+                                    </div> 
+                                </ul>
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+			<div class="col-search2">
+				<div class="input-group" style="margin-top: 25px;">
+				<button id='btn-search' class="btn btn-flat btn-success btn-lg" style="height: 34px;padding-top: 0px;padding-bottom: 0px;">
+                    <i class="fa fa-search"></i> | SEARCH</button>
 				</div>
 			</div>
 		</div><!-- /.box-body -->
@@ -143,12 +220,24 @@
 <script>
 $(document).ready(function(){
     //Date picker
-    $('#datepicker').datepicker({
-       showOtherMonths: true,
-       selectOtherMonths: true,
-       dateFormat: 'dd-mm-yy', 
-       minDate: 0,
+    if (screen.width <=650) {
+        $('#datepicker').datepicker({
+           showOtherMonths: true,
+           selectOtherMonths: true,
+           dateFormat: 'dd-mm-yy', 
+           minDate: 0,
+           numberOfMonths: 1,
     });
+    }else{
+        $('#datepicker').datepicker({
+           showOtherMonths: true,
+           selectOtherMonths: true,
+           dateFormat: 'dd-mm-yy', 
+           minDate: 0,
+           numberOfMonths: 2,
+    });
+    };
+
     $(".bandara").select2();
     var bandara = [] ;
     $.get( base_url+'assets/ajax/iata_bandara.json', function(data) {
@@ -349,4 +438,78 @@ $(document).ready(function(){
     });
     
 });
-</script>
+
+/*passanger*/
+function adultplus() {
+        var adult = parseFloat(document.form.adult.value);
+        var triggerPass = parseFloat(document.form.triggerPass.value);
+        if (triggerPass <=6) {
+            var countadult=adult+1;
+            form.adult.value=countadult
+        }else {
+           $('[data-toggle="popover"]').popover();
+        };
+    }
+function adultmin() {
+        var adult = parseFloat(document.form.adult.value);
+        var countadult=adult-1;
+        if (countadult >=0) {
+            form.adult.value=countadult
+        };
+    }
+
+function childplus() {
+        var child = parseFloat(document.form.child.value);
+        var triggerPass = parseFloat(document.form.triggerPass.value);
+        if (triggerPass <=6) {
+            var countchild=child+1;
+            form.child.value=countchild
+        }else {
+           $('[data-toggle="popover"]').popover();
+        };
+    }
+function childmin() {
+        var child = parseFloat(document.form.child.value);
+        var countchild=child-1;
+        if (countchild >= 0) {
+            form.child.value=countchild
+        };
+    }
+function infantplus() {
+        var infant = parseFloat(document.form.infant.value);
+        var adult = parseFloat(document.form.adult.value);
+        if (infant < adult) {
+             var countinfant=infant+1;
+            form.infant.value=countinfant
+        }else {
+            $('[data-toggle="popover"]').popover();
+        };
+    }
+function infantmin() {
+        var infant = parseFloat(document.form.infant.value);
+        var countinfant=infant-1;
+            if (countinfant >=0) {
+        form.infant.value=countinfant
+    };
+    }
+function startUser(){
+    interval=setInterval("user()",1)
+  }
+function user(){
+    adult=document.form.adult.value;
+    child=document.form.child.value;
+    infant=document.form.infant.value;
+    document.form.passanger.value=(adult*1)+(child*1)+(infant*1)
+    document.form.triggerPass.value=(adult*1)+(child*1)
+  }
+function stopUser(){
+      clearInterval(interval)
+    }
+
+$('[data-toggle="popover"][data-timeout]').on('shown.bs.popover', function() {
+                this_popover = $(this);
+                setTimeout(function () {
+                    this_popover.popover('hide');
+                }, $(this).data("timeout"));
+            });
+  </script> 
