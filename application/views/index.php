391deb47126950c11f465d526787a7cd4d9c9043
Dita Mahardika S <mahardikasuryadi@gmail.com>
2017-02-15 16:25:39 +0700

    divider menu market
    penambahan garis pembatas

--- application/views/index.php
+++ application/views/index.php
@@ -63,9 +63,10 @@
 		             <li class="dropdown">
 		              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Market <span class="caret"></span></a>
 		              <ul class="dropdown-menu" role="menu">
-		                <li><a href="<?php echo base_url().'ppob/tagihan' ?>">Cek Tagihan</a></li>
 		                <li><a href="<?php echo base_url().'ppob/pulsa' ?>">Pulsa HP</a></li>
 		                <li><a href="<?php echo base_url().'ppob/pln' ?>">Token PLN</a></li>
+		                <li class="divider"></li>
+		                <li><a href="<?php echo base_url().'ppob/tagihan' ?>">Cek Tagihan</a></li>
 		              </ul>
 		            </li>
 		            <?php if($this->ion_auth->logged_in()){ ?>
