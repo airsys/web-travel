ca9e100e68f0983f70295d0371114e2d86cfd8db
pras <frajofic@gmail.com>
2017-03-07 17:03:55 +0700

    change logo 1. logo yang di atas diganti payman.

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -9,7 +9,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>INDSITI | Admin Page</title>
+  <title>PAYMAN | Admin Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -54,7 +54,7 @@
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="#" class="navbar-brand"><b>IND</b>SITI<sup>beta</sup></a>
+          <a href="#" class="navbar-brand"><b>PAY</b>MAN<sup>beta</sup></a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
